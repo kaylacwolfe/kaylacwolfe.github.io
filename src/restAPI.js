0162b23f7749a8382f9aa5b8dea55e40a5fc9f09
Kayla Wolfe <83608133+kaylacwolfe@users.noreply.github.com>
2023-04-13 17:01:22 -0600

    Update restAPI.js

--- src/restAPI.js
+++ src/restAPI.js
@@ -1,8 +1,6 @@
-var APIKey = "RYUXmqBLH4By9Xh8THQd9QNRr";
-
 async function getTime(){
   console.info("calling getTime()");
-  const response = await fetch('https://clever-web.fcgov.com/bustime/api/v3/format=json/gettime?key=' + APIKey);
+  const response = await fetch('https://clever-web.fcgov.com/bustime/api/v3/format=json/gettime?key=RYUXmqBLH4By9Xh8THQd9QNRr');
   const myJson = await response.json();
   console.info(myJson);
 }
