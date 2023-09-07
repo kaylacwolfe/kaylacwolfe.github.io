9a3fc20433672a5d4724d78c5243039849037a1e
kaylacwolfe <kayla.wolfe@colostate.edu>
2023-09-07 13:20:56 -0600

    Fixed contact functionality

--- home.js
+++ home.js
@@ -1,13 +1,21 @@
+function openModal(){
+    document.getElementById('contact').style.display='block'
+}
+
+function closeModal(){
+    document.getElementById('contact').style.display='none'
+}
+
 function sendEmail(){
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "kaylacwolfe@icloud.com",
         Password : "1F0CAAB91241E60547F09F52827D205B5115",
         To : 'kaylacwolfe@icloud.com',
-        From : document.getElementById("email").value,
+        From : 'kaylacwolfe@icloud.com',
         Subject : "An Email From Your Site",
         Body : document.getElementById("message").value
     }).then(
-      message => alert(message)
+      message => alert('Sent Successfully!')
     );
 }
